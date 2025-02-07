a05e2444f61109346ad898f213f88cddec6cb3b1
Art0riasss <guramravshan@gmail.com>
2025-02-07 11:47:51 +0300

    did state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import Shadow from "./components/shadow";
 import anime from "./img/Episode_23.png";
 import './styles/main.css'
 class App extends React.Component {
-  clicked(){console.log('you clicked on it')}
+    constructor(props) {
+        super(props);
+        this.clickInput = this.clickInput.bind(this);
+        this.state = {
+            userData: '',
+            placeHolder: ''
+        }
+    }
   render() {
     return (
         <div>
+            <h2>{this.state.userData}</h2>
+            <input type='text' onChange={event => this.setState({ userData: event.target.value })}/>
           <button onClick={this.clicked}>Click Me!</button>
       <h1>App</h1>
           <Shadow link={anime} />
     </div>
     )
   }
+    clickInput(){this.setState({placeHolder: ''})}
+    clicked(){console.log('you clicked on it')}
 }
 
 
